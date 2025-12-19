c584769ca4a2fa2c9cec986f4234439f699b79cb
rafael <rafaeltorresomega6@gmail.com>
2025-12-18 23:55:03 -0300

    feat: função de remover implementada

--- untitled/src/main/java/Main.java
+++ untitled/src/main/java/Main.java
@@ -14,19 +14,18 @@ public class Main {
                  System.out.println("2 - show task list");
                  System.out.println("3 - update task status");
                  System.out.println("4 - show in order of priority");
-                 System.out.println("5 - exit");
+                 System.out.println("5 - remove task");
+                 System.out.println("6 - exit");
                  System.out.print("type your choice: ");
                  option = enter.nextInt();
                  enter.nextLine();
 
                  switch (option){
-                     case 1 -> {
-                         task.addTask();
-                     }
+                     case 1 -> task.addTask();
 
-                     case 2 -> {
-                         task.showTask();
-                     }
+
+                     case 2 -> task.showTask();
+
 
                      case 3 -> {
                          System.out.print("type id to update status: ");
@@ -35,16 +34,18 @@ public class Main {
                          task.updateStatus(id);
                      }
 
-                     case 4 -> {
-                         task.priorityOrdem();
-                     }
+                     case 4 -> task.priorityOrdem();
 
                      case 5 -> {
-                         System.out.println("leaving...");
+                         System.out.print("type id to remove task: ");
+                         int id = enter.nextInt();
+
+                         task.removeTask(id);
                      }
-                     default -> {
-                         System.out.println("invalid number");
-                     }
+                     case 6 -> System.out.println("leaving...");
+
+                     default -> System.out.println("invalid number");
+
                  }
 
 
@@ -52,7 +53,7 @@ public class Main {
                  System.out.println("type a valid number");
                  enter.nextLine();
              }
-         }while (option != 5 );
+         }while (option != 6 );
          System.out.println("thanks so much, come back soon");
     }
 }
--- untitled/src/main/java/TaskManager.java
+++ untitled/src/main/java/TaskManager.java
@@ -1,80 +1,88 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
 
 public class TaskManager {
+
     List<Task> taskList = new ArrayList<>();
     Scanner enter = new Scanner(System.in);
     int idCounter;
 
-    private String askForStatus(){
+    public Priority askForPriority(){
+        System.out.println("1 - HIGH");
+        System.out.println("2 - MID");
+        System.out.println("3 - LOW");
+        System.out.print("type priority of task: ");
+        int choosePriority = enter.nextInt();
+        enter.nextLine();
+
+        return switch (choosePriority){
+            case 1 -> Priority.HIGH;
+            case 2 -> Priority.MID;
+            default -> Priority.LOW;
+        };
+    }
+
+    public Status askForStatus(){
         System.out.println("1 - to do");
         System.out.println("2 - in progress");
         System.out.println("3 - done");
-        System.out.print("choose the status: ");
-        int stats = enter.nextInt();
+        System.out.print("type Status of task: ");
+        int chooseStatus = enter.nextInt();
         enter.nextLine();
 
-        Status status = switch (stats){
+        return switch (chooseStatus){
             case 1 -> Status.TODO;
             case 2 -> Status.INPROGRESS;
-            case 3 -> Status.DONE;
-            default -> Status.TODO; // Mesmo problema da primeira parte
+            default -> Status.DONE;
         };
-        return null;
     }
 
     public void addTask(){
         idCounter ++;
+
         System.out.print("type description: ");
         String description = enter.nextLine();
 
-        System.out.println("1 - high");
-        System.out.println("2 - mid");
-        System.out.println("3 - low");
-        System.out.print("choose the priority: ");
-        int choose = enter.nextInt();
-        enter.nextLine();
-
-        Priority priority = switch (choose) {
-            case 1 -> Priority.HIGH;
-            case 2 -> Priority.MID;
-            case 3 -> Priority.LOW;
-            default -> Priority.LOW; // O Java obriga o default aqui
-        };
-
-        askForStatus();
+        Priority p = askForPriority();
+        Status s = askForStatus();
 
         Task task = new Task();
+
         task.setDescription(description);
-        task.setPriority(priority);
+        task.setPriority(p);
+        task.setTaskDone(s);
         task.setId(idCounter);
 
         taskList.add(task);
     }
 
     public void showTask(){
-        for (Task i : taskList){
-            System.out.println(i);
+        for(Task t : taskList){
+            System.out.println(t);
         }
     }
 
     public void updateStatus(int id){
-        boolean found = false;
-        String newStats = "";
-        for(Task t : taskList){
-            if(id == t.getId()){
-
+        for (Task t : taskList){
+            if (t.getId() == id){
+                Status s = askForStatus();
+                t.setTaskDone(s);
             }
-        }
-        if (!found ){
-            System.out.println("error: id task not found");
         }
     }
 
     public void priorityOrdem(){
-
-        System.out.println("--- tasks ordered by priority ---");
+        System.out.println("----- ordered by priority");
         taskList.sort(Comparator.comparing(Task::getPriority));
         showTask();
-
     }
+
+    public void removeTask(int id){
+        taskList.removeIf(task -> task.getId() == id);
+    }
+
+
+
 }
